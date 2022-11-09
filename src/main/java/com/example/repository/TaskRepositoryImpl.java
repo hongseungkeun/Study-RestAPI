db8ff7515684871b881c5a3132a4752d401ab499
hongseungkeun <eiziong1@gmail.com>
2022-11-10 00:06:08 +0900

    RestAPI-ToDoList 레포지토리

---
+++ src/main/java/com/example/repository/TaskRepositoryImpl.java
@@ -0,0 +1,56 @@
+package com.example.repository;
+
+import com.example.domain.Task;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class TaskRepositoryImpl implements TaskRepository{
+    private static final Map<Long, Task> tasks = new ConcurrentHashMap<>();
+
+    @Override
+    public List<Task> findAll() {
+        return new ArrayList<>(tasks.values());
+    }
+
+    @Override
+    public ResponseEntity<Task> readTask(Long id) {
+        if(!tasks.containsKey(id)){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(tasks.get(id), HttpStatus.OK);
+    }
+
+    @Override
+    public Task createTask(Task task) {
+        Task newTask = new Task(Generated.incrementId(),task.getTitle());
+        tasks.put(newTask.getId(), newTask);
+        return newTask;
+    }
+
+    @Override
+    public ResponseEntity<Task> updateTask(Long id, Task task) {
+        if(!tasks.containsKey(id)){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        task.setId(id);
+        tasks.put(task.getId(),task);
+        return new ResponseEntity<>(task, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Task> deleteTask(Long id) {
+        if(!tasks.containsKey(id)){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Task task = tasks.get(id);
+        tasks.remove(id);
+        return new ResponseEntity<>(task, HttpStatus.NO_CONTENT);
+    }
+}
