c642476dfc252faeedcd4ec895cf090f29044a08
hongseungkeun <eiziong1@gmail.com>
2022-11-21 19:09:00 +0900

    @ApiOperation 어노테이션 이용한 API 주석 추가

--- src/main/java/com/example/controller/TaskController.java
+++ src/main/java/com/example/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import com.example.domain.Task;
 import com.example.service.TaskService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,28 +19,33 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @ApiOperation(value = "ToDo 목록 얻기")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Task> read(){
         return taskService.findAll();
     }
 
+    @ApiOperation(value = "ToDo 상세 조회하기")
     @GetMapping("/{id}")
     public ResponseEntity<Task> read(@PathVariable Long id){
         return taskService.readTask(id);
     }
 
+    @ApiOperation(value = "ToDo 생성하기 ")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Task create(@RequestBody Task task){
         return taskService.createTask(task);
     }
 
+    @ApiOperation(value = "ToDo 제목 수정하기")
     @RequestMapping(value = "/{id}", method = {RequestMethod.PATCH, RequestMethod.PUT})
     public ResponseEntity<Task> update(@PathVariable Long id, @RequestBody Task task){
         return taskService.updateTask(id, task);
     }
 
+    @ApiOperation(value = "ToDo 삭제하기")
     @DeleteMapping("/{id}")
     public ResponseEntity<Task> delete(@PathVariable Long id){
         return taskService.deleteTask(id);
