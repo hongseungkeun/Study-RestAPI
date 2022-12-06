4f45eabeadf09006066d5c23e70d92be719575eb
hongseungkeun <eiziong1@gmail.com>
2022-12-06 22:18:30 +0900

    test: AtomicLong 멀티쓰레드 환경 동시성 체크

--- src/test/java/com/example/repositoryTest/GeneratedTest.java
+++ src/test/java/com/example/repositoryTest/GeneratedTest.java
@@ -3,7 +3,6 @@ package com.example.repositoryTest;
 import com.example.repository.Generated;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class GeneratedTest {
     @Test
@@ -12,14 +11,20 @@ public class GeneratedTest {
         Runnable run1 = new CheckThread();
         Runnable run2 = new CheckThread();
         Runnable run3 = new CheckThread();
+        Runnable run4 = new CheckThread();
+        Runnable run5 = new CheckThread();
 
         Thread thread1 = new Thread(run1);
         Thread thread2 = new Thread(run2);
         Thread thread3 = new Thread(run3);
+        Thread thread4 = new Thread(run4);
+        Thread thread5 = new Thread(run5);
 
         thread1.start();
         thread2.start();
         thread3.start();
+        thread4.start();
+        thread5.start();
     }
     class CheckThread implements Runnable{
 
