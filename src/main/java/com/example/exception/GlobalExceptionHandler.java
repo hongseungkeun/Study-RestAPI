af94e2f1304db33d3e10957a48f34c6e31e50b2c
hongseungkeun <eiziong1@gmail.com>
2022-12-06 22:19:57 +0900

    refactor: id를 찾지 못했을 시 에러 핸들 추가

---
+++ src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleException(TaskNotFoundException e){
+        return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+}
