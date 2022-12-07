c573443d55aa82eee3d70d7e1938c688607d37a0
hongseungkeun <eiziong1@gmail.com>
2022-12-07 18:55:49 +0900

    refactor: 변수에 final 추가

--- src/main/java/com/example/repository/Generated.java
+++ src/main/java/com/example/repository/Generated.java
@@ -3,7 +3,7 @@ package com.example.repository;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Generated {
-    private static AtomicLong sequence = new AtomicLong();
+    private static final AtomicLong sequence = new AtomicLong();
 
     public static Long incrementId(){
         return sequence.addAndGet(1);
