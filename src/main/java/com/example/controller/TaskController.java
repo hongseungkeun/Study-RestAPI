edd06f9aa863211cccea265ca9077eea5c880065
hongseungkeun <eiziong1@gmail.com>
2022-12-07 18:55:13 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/com/example/controller/TaskController.java
+++ src/main/java/com/example/controller/TaskController.java
@@ -3,7 +3,6 @@ package com.example.controller;
 import com.example.domain.Task;
 import com.example.service.TaskService;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
--- src/main/java/com/example/service/TaskServiceImpl.java
+++ src/main/java/com/example/service/TaskServiceImpl.java
@@ -2,8 +2,6 @@ package com.example.service;
 
 import com.example.domain.Task;
 import com.example.repository.TaskRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
